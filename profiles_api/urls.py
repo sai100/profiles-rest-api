ec2b933c77d25c06ca050d68f76f3966a767e27a
Sai <saisundar100@gmail.com>
2023-01-01 23:21:17 +0530

    Added Profiles ViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profiles', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view()),
